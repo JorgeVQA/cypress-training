b7f3e6bb0a973288cebb97b287be9e2341d55176
Jorge Velasquez <jorge.velasquez@perficient.com>
2022-04-27 22:13:51 -0500

    6. Análisis de código estatico - uso del ESLint (#4)
    Co-authored-by: Jorge Velasquez <jorge.velasquez@talosdigital.com>

--- cypress/integration/google.spec.ts
+++ cypress/integration/google.spec.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe("This is my first cypress test", () => {
-    it("should have a title", () => {
-        cy.visit("https://www.google.com/");
-        cy.title().should("eq", "Google");
-    });
+  it("should have a title", () => {
+    cy.visit("https://www.google.com/");
+    cy.title().should("eq", "Google");
+  });
 });
