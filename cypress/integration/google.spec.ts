f8e7495e7f86978fa2dac933e5eb3357edec7ad0
Jorge Velasquez <jorge.velasquez@perficient.com>
2022-04-29 10:51:11 -0500

    7-Configurar Integracion Continua CI nvmrc (#6)
    * 7-Configurar Integracion Continua CI nvmrc
    
    * 7-Configurar Integracion Continua CI nvmrc - Fix package_json
    
    * 7-Configurar Integracion Continua CI nvmrc - Fix cypress_json
    
    Co-authored-by: Jorge Velasquez <jorge.velasquez@talosdigital.com>

--- cypress/integration/google.spec.ts
+++ cypress/integration/google.spec.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// /<reference types="cypress" />
 describe("This is my first cypress test", () => {
   it("should have a title", () => {
     cy.visit("https://www.google.com/");
