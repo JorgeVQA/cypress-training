d933c4a5789273b72e0b8e4493fca9c8588d75f4
Jorge Velasquez <jorge.velasquez@perficient.com>
2022-09-07 14:44:28 -0500

    11 AAA Pattern - Applied to test cases (#10)
    * 11 AAA Pattern - Applied to test cases
    
    * 11 AAA Pattern - Applied to test cases fix code
    
    * Update dresses-list.cy.ts
    
    * 11 AAA Pattern - Applied to All Test Cases and Fixes Done
    
    * Update dresses-list.cy.ts
    
    * fix dresses 3
    
    * fix dresses 4
    
    Co-authored-by: Jorge Velasquez <jorge.velasquez@talosdigital.com>

---
+++ cypress/integration/dresses-list.cy.ts
@@ -0,0 +1,29 @@
+// /<reference types="cypress" />
+
+import {MenuContentPage, DressesListPage} from "../page/index";
+
+describe("the user navigates to the dresses page should", () => {
+  let menuContentPage: MenuContentPage;
+  let dressesListPage: DressesListPage;
+  let arrayDressNameList: Array<string>;
+
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    dressesListPage = new DressesListPage();
+    arrayDressNameList = [
+      "Printed Dress",
+      "Printed Dress",
+      "Printed Summer Dress",
+      "Printed Summer Dress",
+      "Printed Chiffon Dress",
+    ];
+    menuContentPage.visitMenuContentPage();
+  });
+
+  it("show the available dresses", () => {
+    // ... realiza la prueba
+    menuContentPage.goToDressesMenu();
+    dressesListPage.validateItemsNumber(arrayDressNameList.length);
+    dressesListPage.validateItemsNames(arrayDressNameList);
+  });
+});
--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,19 +1,28 @@
+import { StringMatcher } from "cypress/types/net-stubbing";
+
 class MenuContentPage {
+  private dressesMenu: string; // AAA Pattern
   private tShirtMenu: string;
-  private menuContentPageURL: string
+  private menuContentPageURL: string;
 
   constructor() {
-    this.menuContentPageURL = "http://automationpractice.com/"
-    this.tShirtMenu = ".sf-menu > li > a[title=T-shirts]";  
+    this.dressesMenu = ".sf-menu > li > a[title=Dresses]"; // AAA Pattern
+    this.menuContentPageURL = "http://automationpractice.com/";
+    this.tShirtMenu = ".sf-menu > li > a[title=T-shirts]";
+  }
+
+  public goToDressesMenu(): void {
+    // AAA Pattern
+    cy.get(this.dressesMenu).click();
   }
 
   public visitMenuContentPage(): void {
-    cy.visit(this.menuContentPageURL)
+    cy.visit(this.menuContentPageURL);
   }
 
   public goToTShirtMenu(): void {
-    cy.get(this.tShirtMenu).click()
+    cy.get(this.tShirtMenu).click();
   }
 }
 
-export { MenuContentPage }
+export { MenuContentPage };
